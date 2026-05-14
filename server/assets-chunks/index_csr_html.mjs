1f046c3d93d14a2f63e83bfa81eb00504da0e4f7
KushnerchukIvan <ikushnerchuk2005@gmail.com>
2026-05-14 19:14:41 +0300

    Auto-generated commit

---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>IvanDev</title>
+  <base href="https://KushnerchukIvan.github.io/resume/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<style>html,body{margin:0;padding:0;width:100%;height:100%;background:linear-gradient(to right,#000,#000028)}</style><link rel="stylesheet" href="styles-EOXRU7PR.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-EOXRU7PR.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-NVEKBYSQ.js" type="module"></script></body>
+</html>
+`;
